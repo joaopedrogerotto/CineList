5ea5d151e91a5548fc003b3bf93b15b15f2a102d
jpgerotto <jpgertto.1204fernadesz@gmail.com>
2026-09-15 14:02:08 -0300

    feat: dados mockados de filmes

---
+++ CineList/src/data/movie.js
@@ -0,0 +1,122 @@
+export const movies = [
+    {
+        id: 1,
+        title: "Interestelar",
+        genre: "Ficção Científica",
+        year: 2014,
+        rating: 8.7,
+        image: "https://image.tmdb.org/t/p/w500/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg"
+    },
+    {
+        id: 2,
+        title: "O Poderoso Chefão",
+        genre: "Drama",
+        year: 1972,
+        rating: 9.2,
+        image: "https://image.tmdb.org/t/p/w500/3bhkrj58Vtu7enYsRolD1fZdja1.jpg"
+    },
+    {
+        id: 3,
+        title: "Batman: O Cavaleiro das Trevas",
+        genre: "Ação",
+        year: 2008,
+        rating: 9.0,
+        image: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg"
+    },
+    {
+        id: 4,
+        title: "Pulp Fiction",
+        genre: "Crime",
+        year: 1994,
+        rating: 8.9,
+        image: "https://image.tmdb.org/t/p/w500/d5iIlFn5s0ImszYzBPb8JPIfbXD.jpg"
+    },
+    {
+        id: 5,
+        title: "A Origem",
+        genre: "Ficção Científica",
+        year: 2010,
+        rating: 8.8,
+        image: "https://image.tmdb.org/t/p/w500/oYuLEt3zVCKq57qu2F8dT7NIa6f.jpg"
+    },
+    {
+        id: 6,
+        title: "Forrest Gump",
+        genre: "Drama",
+        year: 1994,
+        rating: 8.8,
+        image: "https://image.tmdb.org/t/p/w500/arw2vcBveWOVZr6pxd9XTd1TdQa.jpg"
+    },
+    {
+        id: 7,
+        title: "Matrix",
+        genre: "Ficção Científica",
+        year: 1999,
+        rating: 8.7,
+        image: "https://image.tmdb.org/t/p/w500/f89U3ADr1oiB1s9GkdPOEpXUk5H.jpg"
+    },
+    {
+        id: 8,
+        title: "O Senhor dos Anéis: O Retorno do Rei",
+        genre: "Fantasia",
+        year: 2003,
+        rating: 9.0,
+        image: "https://image.tmdb.org/t/p/w500/rCzpDGLbOoPwLjy3OAm5NUPOTrC.jpg"
+    },
+    {
+        id: 9,
+        title: "Clube da Luta",
+        genre: "Drama",
+        year: 1999,
+        rating: 8.8,
+        image: "https://image.tmdb.org/t/p/w500/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg"
+    },
+    {
+        id: 10,
+        title: "Vingadores: Ultimato",
+        genre: "Ação",
+        year: 2019,
+        rating: 8.3,
+        image: "https://image.tmdb.org/t/p/w500/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg"
+    },
+    {
+        id: 11,
+        title: "Homem-Aranha: No Aranhaverso",
+        genre: "Animação",
+        year: 2018,
+        rating: 8.4,
+        image: "https://image.tmdb.org/t/p/w500/8Vt6mWEReuy4Of61Lnj5Xj704m8.jpg"
+    },
+    {
+        id: 12,
+        title: "O Rei Leão",
+        genre: "Animação",
+        year: 1994,
+        rating: 8.5,
+        image: "https://image.tmdb.org/t/p/w500/sKCr78MXSLixwmZ8DyJLrpMsd15.jpg"
+    },
+    {
+        id: 13,
+        title: "Toy Story",
+        genre: "Animação",
+        year: 1995,
+        rating: 8.3,
+        image: "https://image.tmdb.org/t/p/w500/uXDfjJbdP4ijW5hWSBrPrlKpxab.jpg"
+    },
+    {
+        id: 14,
+        title: "Gladiador",
+        genre: "Ação",
+        year: 2000,
+        rating: 8.5,
+        image: "https://image.tmdb.org/t/p/w500/ty8TGRuvJLPUmAR1H1nRIsgwvim.jpg"
+    },
+    {
+        id: 15,
+        title: "O Iluminado",
+        genre: "Terror",
+        year: 1980,
+        rating: 8.4,
+        image: "https://image.tmdb.org/t/p/w500/9fgh3Ns1iRzlQNYuJyK0ARQZU7w.jpg"
+    }
+];

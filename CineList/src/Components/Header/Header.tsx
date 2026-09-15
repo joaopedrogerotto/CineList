d07a272707f87056b50c78c74aff80289ab210c5
jpgerotto <jpgertto.1204fernadesz@gmail.com>
2026-09-15 13:59:13 -0300

    feat: header

---
+++ CineList/src/Components/Header/Header.tsx
@@ -0,0 +1,26 @@
+import { Link, NavLink } from "react-router-dom";
+import "./Header.css"
+
+
+function Header(){
+    return(
+        <header className="header">
+            <div className="headerContainer">
+                <nav className="nav">
+                    <NavLink 
+                        to="/filmes" 
+                        className={({isActive}) => isActive ? "navLink active" :  "navLink"}>
+                            Filmes
+                    </NavLink>
+                    <NavLink 
+                        to="/favoritos" 
+                        className={({isActive}) => isActive ? "navLink active" :  "navLink"}>
+                            Favoritos
+                    </NavLink>
+                </nav>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
